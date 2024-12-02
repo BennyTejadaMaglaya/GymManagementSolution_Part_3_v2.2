47e40d107ad1b5a0c1b456aa3d587b755c8ce332
Teejay Madlangbayan <40991102+BennyTejadaMaglaya@users.noreply.github.com>
2024-12-02 06:15:33 -0500

    Add files via upload

---
+++ GymManagementSolution_Part_3_v2.2/GymManagement/Controllers/FitnessCategoryController.cs
@@ -0,0 +1,210 @@
+﻿using GymManagement.CustomControllers;
+using GymManagement.Data;
+using GymManagement.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace GymManagement.Controllers
+{
+    public class FitnessCategoryController : CognizantController
+    {
+        private readonly GymContext _context;
+
+        public FitnessCategoryController(GymContext context)
+        {
+            _context = context;
+        }
+
+        // GET: FitnessCategory
+        public async Task<IActionResult> Index()
+        {
+            var fitnessCategories = await _context.FitnessCategories
+                .Include(c=>c.ExerciseCategories)
+                .AsNoTracking()
+                .ToListAsync();
+            return View(fitnessCategories);
+        }
+
+        // GET: FitnessCategory/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var fitnessCategory = await _context.FitnessCategories
+                .AsNoTracking()
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (fitnessCategory == null)
+            {
+                return NotFound();
+            }
+
+            return View(fitnessCategory);
+        }
+
+        // GET: FitnessCategory/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: FitnessCategory/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Category")] FitnessCategory fitnessCategory)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    _context.Add(fitnessCategory);
+                    await _context.SaveChangesAsync();
+                    var returnUrl = ViewData["returnURL"]?.ToString();
+                    if (string.IsNullOrEmpty(returnUrl))
+                    {
+                        return RedirectToAction(nameof(Index));
+                    }
+                    return Redirect(returnUrl);
+                }
+            }
+            catch (DbUpdateException)
+            {
+                ModelState.AddModelError("", "Unable to save changes. Try again, and if the problem persists see your system administrator.");
+            }
+
+            return View(fitnessCategory);
+        }
+
+        // GET: FitnessCategory/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var fitnessCategory = await _context.FitnessCategories.FindAsync(id);
+            if (fitnessCategory == null)
+            {
+                return NotFound();
+            }
+            return View(fitnessCategory);
+        }
+
+        // POST: FitnessCategory/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id)
+        {
+            //Go get the Doctor to update
+            var fitnessCategoryToUpdate = await _context.FitnessCategories
+                .FirstOrDefaultAsync(p => p.ID == id);
+
+            //Check that you got it or exit with a not found error
+            if (fitnessCategoryToUpdate == null)
+            {
+                return NotFound();
+            }
+
+            //Try updating it with the values posted
+            if (await TryUpdateModelAsync<FitnessCategory>(fitnessCategoryToUpdate, "",
+                d => d.Category))
+            {
+                try
+                {
+                    await _context.SaveChangesAsync();
+                    var returnUrl = ViewData["returnURL"]?.ToString();
+                    if (string.IsNullOrEmpty(returnUrl))
+                    {
+                        return RedirectToAction(nameof(Index));
+                    }
+                    return Redirect(returnUrl);
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!FitnessCategoryExists(fitnessCategoryToUpdate.ID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                catch (DbUpdateException)
+                {
+                    ModelState.AddModelError("", "Unable to save changes. Try again, and if the problem persists see your system administrator.");
+                }
+
+            }
+            return View(fitnessCategoryToUpdate);
+        }
+
+        // GET: FitnessCategory/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var fitnessCategory = await _context.FitnessCategories
+                .AsNoTracking()
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (fitnessCategory == null)
+            {
+                return NotFound();
+            }
+
+            return View(fitnessCategory);
+        }
+
+        // POST: FitnessCategory/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var fitnessCategory = await _context.FitnessCategories.FindAsync(id);
+            try
+            {
+                if (fitnessCategory != null)
+                {
+                    _context.FitnessCategories.Remove(fitnessCategory);
+                }
+
+                await _context.SaveChangesAsync();
+                var returnUrl = ViewData["returnURL"]?.ToString();
+                if (string.IsNullOrEmpty(returnUrl))
+                {
+                    return RedirectToAction(nameof(Index));
+                }
+                return Redirect(returnUrl);
+            }
+            catch (DbUpdateException dex)
+            {
+                if (dex.GetBaseException().Message.Contains("FOREIGN KEY constraint failed"))
+                {
+                    ModelState.AddModelError("", "Unable to Delete Fitness Category. " +
+                        "Remember, you cannot delete a Category if there are Group Classes in it.");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Unable to save changes. Try again, and if the problem persists see your system administrator.");
+                }
+            }
+            return View(fitnessCategory);
+
+        }
+
+        private bool FitnessCategoryExists(int id)
+        {
+            return _context.FitnessCategories.Any(e => e.ID == id);
+        }
+    }
+}
